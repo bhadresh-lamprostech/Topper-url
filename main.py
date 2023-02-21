65f98cf903b345cf5a1de03bac762778faaa8a43
bhadresh-lamprostech <bhadresh.lamprostech@gmail.com>
2023-02-21 12:54:07 +0530

    final commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ output_wb = Workbook()
 output_ws = output_wb.active
 
 # Write column headings to output worksheet
-output_ws.append(["Question", "Choice 1", "Choice 2", "Choice 3", "Choice 4"])
+output_ws.append(["Question", "Choice 1", "Choice 2", "Choice 3", "Choice 4", "sub 1","sub 2","sub 3", "sub 4" ])
 
 for row in input_ws.iter_rows(min_row=2, values_only=True):
     url = row[0]
@@ -29,6 +29,14 @@ for row in input_ws.iter_rows(min_row=2, values_only=True):
     choices = soup.find_all("div", class_="Option_content__2ZU_b")
     choice_texts = [choice.get_text() for choice in choices]
 
+    # If there are less than 4 choices, fill the remaining choices with "N/A"
+    while len(choice_texts) < 4:
+        choice_texts.append("N/A")
+
+    #find all breadcrumbs and extract the text content
+    subs = soup.find_all("span", class_="Breadcrumb_name__erY_f ellipsis")
+    sub_texts = [sub.get_text() for sub in subs]
+
     # Rearrange choices based on their order on the page
     ordered_choices = [None] * 4
     for i, choice in enumerate(choices):
@@ -36,9 +44,15 @@ for row in input_ws.iter_rows(min_row=2, values_only=True):
         index = choice_texts.index(choice_text)
         ordered_choices[index] = choice_text
 
-    # Write the question and choices to the output worksheet
-    output_ws.append([question] + choice_texts)
+    # Rearrange subs based on their order on the page
+    ordered_subs = [None] * 5
+    for i, sub in enumerate(subs):
+        sub_text = sub.get_text()
+        index = sub_texts.index(sub_text)
+        ordered_subs[index] = sub_text
 
+    # Write the question, choices, and subs to the output worksheet
+    output_ws.append([question] + choice_texts + sub_texts)
 
     driver.quit()
 
